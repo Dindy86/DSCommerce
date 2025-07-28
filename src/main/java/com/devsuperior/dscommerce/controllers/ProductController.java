d11829a7475b756b72765a3d73461a1e1bd6ca1f
Dindy Pierre <ing.pierredindy@gmail.com>
2025-07-28 11:21:01 -0300

    Add GET products paged endpoint

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.controllers;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,12 @@ public class ProductController {
     @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 }
