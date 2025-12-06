c49f3b4c9459156fcb7bfbafaea38a86cbf66f81
Dindy Pierre <ing.pierredindy@gmail.com>
2025-12-06 09:57:24 -0300

    Add ProductDTO with Categories

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,9 +1,14 @@
 package com.devsuperior.dscommerce.dto;
 
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDTO {
     private Long id;
@@ -15,6 +20,8 @@ public class ProductDTO {
     @Positive(message = "The price must be positive")
     private Double price;
     private String imgUrl;
+    @NotEmpty(message = "categories field can't be empty")
+    List<CategoryDTO> categories = new ArrayList<>();
 
     public ProductDTO() {
     }
@@ -33,6 +40,9 @@ public class ProductDTO {
         description = entity.getDescription();
         price = entity.getPrice();
         imgUrl = entity.getImgUrl();
+        for(Category category: entity.getCategories()){
+            categories.add(new CategoryDTO(category));
+        }
     }
 
     public Long getId() {
@@ -53,5 +63,9 @@ public class ProductDTO {
 
     public String getImgUrl() {
         return imgUrl;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
     }
 }
